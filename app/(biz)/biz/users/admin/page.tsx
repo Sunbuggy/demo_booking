ddd3a7775454beef340dfac93e8d97a82b32cdbc
AbenezerKitata <abi4kit@gmail.com>
2024-08-08 15:14:49 -0700

    first user management commit

---
+++ app/(biz)/biz/users/admin/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserManagementPage = () => {
+  return <div>UserManagementPage</div>;
+};
+
+export default UserManagementPage;
+
+// Todo,
+// View Users
+// Add User
+// Edit User's Role and Permissions
+// Delete User
+// View User's Profile
