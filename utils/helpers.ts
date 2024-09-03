94776b501ee6cb68b7e58ba1a3dabafd712f59ac
alexadrao <alexandra@sunbuggyfunrentals.com>
2024-09-03 11:06:04 -0700

    fixing signin redirect

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,16 +1,16 @@
 export const getURL = (path: string = '') => {
   // Check if NEXT_PUBLIC_SITE_URL is set and non-empty. Set this to your site URL in production env.
-  // let url =
-  //   process?.env?.NEXT_PUBLIC_SITE_URL &&
-  //   process.env.NEXT_PUBLIC_SITE_URL.trim() !== ''
-  //     ? process.env.NEXT_PUBLIC_SITE_URL
-  //     : // If not set, check for NEXT_PUBLIC_VERCEL_URL, which is automatically set by Vercel.
-  //       process?.env?.NEXT_PUBLIC_VERCEL_URL &&
-  //         process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ''
-  //       ? process.env.NEXT_PUBLIC_VERCEL_URL
-  //       : // If neither is set, default to localhost for local development.
-  //         'http://localhost:3000/';
-let url=process.env.NODE_ENV==="production"?`${process.env.NEXT_PUBLIC_SITE_URL}`:'http://localhost:3000'
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL &&
+    process.env.NEXT_PUBLIC_SITE_URL.trim() !== ''
+      ? process.env.NEXT_PUBLIC_SITE_URL
+      : // If not set, check for NEXT_PUBLIC_VERCEL_URL, which is automatically set by Vercel.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL &&
+          process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ''
+        ? process.env.NEXT_PUBLIC_VERCEL_URL
+        : // If neither is set, default to localhost for local development.
+          'http://localhost:3000/';
+// let url=process.env.NODE_ENV==="production"?`${process.env.NEXT_PUBLIC_SITE_URL}`:'http://localhost:3000'
   // Trim the URL and remove trailing slash if exists.
   url = url.replace(/\/+$/, '');
   // Make sure to include `https://` when not localhost.
