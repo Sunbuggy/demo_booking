13f046913939ace45899e3ab24f0bf6807f3ee8e
alexadrao <alexandra@sunbuggyfunrentals.com>
2024-09-03 13:04:14 -0700

    fixing get url

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,26 +1,30 @@
 export const getURL = (path: string = '') => {
-  // Check if NEXT_PUBLIC_SITE_URL is set and non-empty. Set this to your site URL in production env.
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL &&
-    process.env.NEXT_PUBLIC_SITE_URL.trim() !== ''
-      ? process.env.NEXT_PUBLIC_SITE_URL
-      : // If not set, check for NEXT_PUBLIC_VERCEL_URL, which is automatically set by Vercel.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL &&
-          process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ''
+  let url = '';
+
+  // Check if we are in production and NEXT_PUBLIC_SITE_URL is set
+  if (process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_SITE_URL) {
+    url = process.env.NEXT_PUBLIC_SITE_URL.trim();
+  } 
+  // If not in production or NEXT_PUBLIC_SITE_URL is not set, fallback to localhost or Vercel URL
+  else {
+    url =
+      process?.env?.NEXT_PUBLIC_VERCEL_URL &&
+      process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ''
         ? process.env.NEXT_PUBLIC_VERCEL_URL
-        : // If neither is set, default to localhost for local development.
-          'http://localhost:3000/';
-// let url=process.env.NODE_ENV==="production"?`${process.env.NEXT_PUBLIC_SITE_URL}`:'http://localhost:3000'
-  // Trim the URL and remove trailing slash if exists.
+        : 'http://localhost:3000';
+  }
+
+  // Trim the URL and remove trailing slash if exists
   url = url.replace(/\/+$/, '');
-  // Make sure to include `https://` when not localhost.
+  // Make sure to include `https://` when not localhost
   url = url.includes('http') ? url : `https://${url}`;
-  // Ensure path starts without a slash to avoid double slashes in the final URL.
+  // Ensure path starts without a slash to avoid double slashes in the final URL
   path = path.replace(/^\/+/, '');
 
-  // Concatenate the URL and the path.
+  // Concatenate the URL and the path
   return path ? `${url}/${path}` : url;
 };
+
 
 export const toDateTime = (secs: number) => {
   var t = new Date(+0); // Unix epoch start.
