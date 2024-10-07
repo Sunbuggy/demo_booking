b794274a9c721a15bcf8f59404e4bd975dfed4a3
AbenezerKitata <abi4kit@gmail.com>
2024-10-07 09:24:27 -0700

    finished location scheduling calendar.

--- app/(biz)/biz/vehicles/[id]/components/location-scheduling.tsx
+++ app/(biz)/biz/vehicles/[id]/components/location-scheduling.tsx
@@ -5,8 +5,22 @@ import dayjs from 'dayjs';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import DialogFactory from '@/components/dialog-factory';
 import SchedulePopup from './schedule-location-popup';
-
+import { createClient } from '@/utils/supabase/client';
+import { fetchVehicleFutureLocationForVehicle } from '@/utils/supabase/queries';
+import { VehicleFutureLocationType } from '../../types';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 const localizer = dayjsLocalizer(dayjs);
+
+type CustomEventsType = {
+  title: string;
+  allDay: boolean;
+  start: Date;
+  end: Date;
+  desc: string;
+};
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const LocationScheduling = ({
   vehicle_id,
@@ -15,26 +29,114 @@ const LocationScheduling = ({
   user_id: string;
   vehicle_id: string;
 }) => {
+  const supabase = createClient();
   const views: View[] = [Views.MONTH, Views.AGENDA];
   const [view, setView] = React.useState<View>(Views.MONTH);
   const [date, setDate] = React.useState(new Date());
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
   const [isOpenDayDialog, setIsOpenDayDialog] = React.useState(false);
+  const [futureLocations, setFutureLocations] = React.useState<
+    VehicleFutureLocationType[]
+  >([]);
+  const [customEventsList, setCustomEventsList] = React.useState<
+    CustomEventsType[]
+  >([]);
 
-  const customEventsList = [
-    {
-      title: 'All Day Event very long title',
-      allDay: true,
-      start: new Date(2024, 10, 0),
-      end: new Date(2024, 10, 1),
-      desc: 'This is a very long description that should be truncated'
-    }
-  ];
+  const fetchFutureLocations = React.useCallback(() => {
+    fetchVehicleFutureLocationForVehicle(supabase, vehicle_id)
+      .then((data) => {
+        setFutureLocations(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching future locations', error);
+      });
+  }, [supabase, vehicle_id]);
+
+  React.useEffect(() => {
+    fetchFutureLocations();
+  }, [fetchFutureLocations]);
+
+  React.useEffect(() => {
+    const channel = supabase
+      .channel('realtime: location scheduling')
+      .on(
+        'postgres_changes',
+        {
+          event: '*',
+          schema: 'public',
+          table: 'vehicle_future_location'
+        },
+        () => {
+          fetchFutureLocations();
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [supabase, fetchFutureLocations]);
+  //   create calendarStyle where it styles past dates differently
+
+  React.useEffect(() => {
+    const customEvents = futureLocations.map((location) => {
+      const futureDate = dayjs(location.future_date).tz(dayjs.tz.guess());
+      return {
+        title: location.future_location ?? '',
+        allDay: true,
+        start: futureDate.toDate(),
+        end: futureDate.toDate(),
+        desc: `
+            Created At: ${location.created_at || 'Unknown'} - \n
+            Created By: ${location.created_by || 'Unknown'} - \n
+            Location: ${location.future_location || 'Unknown'}  \n
+            Date: ${location.future_date || 'Unknown'}  \n
+            `
+      };
+    });
+    setCustomEventsList(customEvents);
+  }, [futureLocations]);
+
   const handleSelectEvent = (event: any) => {
     setIsOpenDayDialog(true);
   };
 
   const handleSelectSlot = (slotInfo: any) => {
+    setSelectedDate(slotInfo.start);
+    const slotDate = new Date(slotInfo.start);
+    if (slotDate < dayjs(new Date()).subtract(1, 'day').toDate()) {
+      alert('Cannot select past dates');
+      return;
+    }
     setIsOpenDayDialog(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsOpenDayDialog(false);
+    fetchFutureLocations();
+  };
+
+  //   Change color of past dates
+  const isPastDay = (date: Date) => {
+    return date < new Date();
+  };
+
+  // datecellwrapper typescript
+
+  const DateCellWrapper = ({
+    children,
+    value
+  }: {
+    children: React.ReactElement;
+    value: Date;
+  }) => {
+    const isPast = isPastDay(value);
+    return React.cloneElement(React.Children.only(children), {
+      style: {
+        ...children.props.style,
+        backgroundColor: isPast ? 'lightgray' : 'inherit'
+      }
+    });
   };
 
   return (
@@ -51,11 +153,16 @@ const LocationScheduling = ({
         defaultView={view}
         defaultDate={new Date()}
         selectable
+        date={date}
         onSelectEvent={handleSelectEvent}
         onSelectSlot={handleSelectSlot}
         onNavigate={(date) => {
           setDate(new Date(date));
         }}
+        components={{
+          dateCellWrapper: DateCellWrapper
+        }}
+        min={new Date()}
       />
 
       <DialogFactory
@@ -67,7 +174,8 @@ const LocationScheduling = ({
           <SchedulePopup
             vehicle_id={vehicle_id}
             user_id={user_id}
-            date={date}
+            date={selectedDate}
+            onClose={handleDialogClose}
           />
         }
       />
--- app/(biz)/biz/vehicles/[id]/components/schedule-location-popup.tsx
+++ app/(biz)/biz/vehicles/[id]/components/schedule-location-popup.tsx
@@ -4,26 +4,49 @@ import { createClient } from '@/utils/supabase/client';
 import { insertIntoVehicleFutureLocation } from '@/utils/supabase/queries';
 import { DialogClose } from '@radix-ui/react-dialog';
 import React from 'react';
-
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 const SchedulePopup = ({
   vehicle_id,
   user_id,
-  date
+  date,
+  onClose
 }: {
   user_id: string;
   vehicle_id: string;
   date: Date;
+  onClose: () => void;
 }) => {
   const { toast } = useToast();
-
+  const [futureLocation, setFutureLocation] = React.useState('');
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
   async function handleSave() {
+    if (futureLocation === '') {
+      toast({
+        title: 'Error',
+        description: 'Please select a location',
+        variant: 'destructive',
+        duration: 3000
+      });
+      return;
+    }
     const supabase = createClient();
-    await insertIntoVehicleFutureLocation(supabase, {
-      created_at: new Date().toISOString(),
+
+    const location = {
+      created_at: dayjs().tz(dayjs.tz.guess()).format('YYYY-MM-DDTHH:mm:ssZ'),
       created_by: user_id,
-      future_date: date.toISOString(),
-      vehicle_id: vehicle_id
-    })
+      future_date: dayjs(date)
+        .tz(dayjs.tz.guess())
+        .format('YYYY-MM-DDTHH:mm:ssZ'),
+      vehicle_id: vehicle_id,
+      future_location: futureLocation
+    };
+    console.log(location);
+    return;
+
+    await insertIntoVehicleFutureLocation(supabase, { ...location })
       .then((data) => {
         toast({
           title: 'Success',
@@ -31,6 +54,7 @@ const SchedulePopup = ({
           variant: 'success',
           duration: 3000
         });
+        onClose();
       })
       .catch((error) => {
         toast({
@@ -43,16 +67,36 @@ const SchedulePopup = ({
   }
 
   return (
-    <div>
-      Selected Date: {date.toDateString()}
-      {/* Select element with 3 values Vegas, SL MI, Pismo Ca */}
-      <select>
-        <option value="Vegas">Las Vegas NV</option>
-        <option value="Silver Lake">Silver Lake MI</option>
-        <option value="Pismo">Pismo Ca</option>
-      </select>
+    <div className="p-4 rounded-lg shadow-md flex flex-col">
+      <div className="mb-4 text-lg font-semibold">
+        Selected Date: {date.toDateString()}
+      </div>
+      <div className="mb-4 flex flex-col">
+        <label
+          htmlFor="location"
+          className="block mb-2 text-sm font-medium text-gray-700"
+        >
+          Select Location
+        </label>
+        <select
+          value={futureLocation}
+          onChange={(e) => setFutureLocation(e.target.value)}
+          id="location"
+          className="block w-full px-3 py-2 text-base leading-6 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        >
+          <option value="">Select Location</option>
+          <option value="Vegas">Las Vegas NV</option>
+          <option value="Silver Lake">Silver Lake MI</option>
+          <option value="Pismo">Pismo Ca</option>
+        </select>
+      </div>
       <DialogClose asChild>
-        <button onClick={handleSave}>Save</button>
+        <button
+          onClick={handleSave}
+          className="px-4 py-2 font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        >
+          Save
+        </button>
       </DialogClose>
     </div>
   );
