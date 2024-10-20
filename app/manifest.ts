a80b35ec20db403e73cf91525c15fb5ad27820f7
AbenezerKitata <abi4kit@gmail.com>
2024-10-20 14:54:29 -0700

    install as a web app test

--- app/manifest.ts
+++ app/manifest.ts
@@ -3,8 +3,8 @@ import type { MetadataRoute } from 'next';
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Sunbuggy PWA',
-    short_name: 'NextPWA',
-    description: 'A Progressive Web App built with Sunbuggy',
+    short_name: 'SBPWA',
+    description: 'A Progressive Web App built by Sunbuggy',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import ChooseAdventure from './(com)/choose-adventure/page';
-
+import { default as InstallSunbuggy } from '@/components/add-to-screen/page';
 export default async function MainPage() {
   return (
     <div>
       <ChooseAdventure />
+      <InstallSunbuggy />
     </div>
   );
 }
