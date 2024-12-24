e1cd242cf020910da5858672573141bf7b91179d
alexandrao <alexandrao@sunbuggyfunrentals.com>
2024-12-24 14:39:35 -0800

    added table for audit log

--- app/(biz)/biz/audit/page.tsx
+++ app/(biz)/biz/audit/page.tsx
@@ -1,10 +1,12 @@
 import { DataTable } from "./components/DataTable";
+import RealtimeTableListener from "./components/TableListener";
 
 export default function AuditLogPage() {
   return (
     <div>
       <h1>Audit Log</h1>
       <DataTable />
+      {/* <RealtimeTableListener tableName="qr_history" /> */}
     </div>
   );
 }
