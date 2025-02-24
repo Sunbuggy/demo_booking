b9ecc391539f52f4f211f8e5e861dd053156f893
jimsunbuggy <jimmy@sunbuggy.com>
2025-02-24 12:35:54 -0800

    switching to Alexandras

--- app/(biz)/biz/fetch_pics/page.tsx
+++ app/(biz)/biz/fetch_pics/page.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import Sometxt from './components/sometxt'
-import Somecrap from './components/somecrap'
+import React from 'react';
+import Sometxt from './components/sometxt';
+import Somecrap from './components/somecrap';
 
 function FetchPics() {
   return (
     <>
-    <div><center>You have reached the fetch pics page - good job!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</center></div>
-   <center><Sometxt></Sometxt></center>
-   <center><Somecrap></Somecrap></center>
+      <div>
+        <center>
+          You have reached the fetch pics page - good
+          job!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </center>
+      </div>
+      <center>
+        <Sometxt></Sometxt>
+      </center>
+      <center>
+        <Somecrap></Somecrap>
+      </center>
     </>
-)
+  );
 }
 
-export default FetchPics
+export default FetchPics;
